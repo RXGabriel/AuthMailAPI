14058881bd61b820924acef439a02492ae81d774
RXGabriel <gabriel2005rf@gmail.com>
2024-01-31 11:05:45 -0300

    Validating the token

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,28 +1,30 @@
-import { Request, Response, NextFunction } from "express"
-import {User} from '../model/User'
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+import dotenv from 'dotenv';
 
-export const Auth ={
-  private:async (req:Request, res:Response, next:NextFunction) =>{
-    let sucess = false
+dotenv.config();
 
-    if(req.headers.authorization){
-      let hash: string = req.headers.authorization.substring(6)
-      let decoded: string = Buffer.from(hash, 'base64').toString()
-      let data: string[] = decoded.split(':')
+export const Auth = {
+  private: async (req: Request, res: Response, next: NextFunction) => {
+    let success = false;
 
-      if(data.length === 2){
-        let hasUser = await User.findOne({ where: {email: data[0] , password: data[1]}})
-        if(hasUser){
-          sucess = true
+    if (req.headers.authorization) {
+      const [authType, token] = req.headers.authorization.split(' ');
+
+      if (authType === 'Bearer') {
+        try {
+          verify(token, process.env.JWT_SECRET_KEY as string);
+          success = true;
+        } catch (err) {
+          console.error('Error verifying token:', err);
         }
       }
     }
 
-    if(sucess){
-      next()
-    }else{
-      res.status(403)
-      res.json({error: 'Acesso negado.'})
+    if (success) {
+      next();
+    } else {
+      res.status(403).json({ error: 'Acesso negado.' });
     }
   }
-}
+};
