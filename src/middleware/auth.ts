55b6992504a1255b7efe80f104a2faa8f616c641
RXGabriel <gabriel2005rf@gmail.com>
2024-01-31 10:19:45 -0300

    Performing Basic Auth authentication

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,8 +1,22 @@
 import { Request, Response, NextFunction } from "express"
+import {User} from '../model/User'
 
 export const Auth ={
-  private:(req:Request, res:Response, next:NextFunction) =>{
+  private:async (req:Request, res:Response, next:NextFunction) =>{
     let sucess = false
+
+    if(req.headers.authorization){
+      let hash: string = req.headers.authorization.substring(6)
+      let decoded: string = Buffer.from(hash, 'base64').toString()
+      let data: string[] = decoded.split(':')
+
+      if(data.length === 2){
+        let hasUser = await User.findOne({ where: {email: data[0] , password: data[1]}})
+        if(hasUser){
+          sucess = true
+        }
+      }
+    }
 
     if(sucess){
       next()
